17f1934cf32886c1f65a1f5435a5d06c5c3bd058
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-23 17:20:08 +0800

    Register Arataki Itto reviewed teams at startup

--- js/features/aloy-reviewed-bootstrap.js
+++ js/features/aloy-reviewed-bootstrap.js
@@ -1,6 +1,8 @@
 import { registerReviewedTeams } from '../data/team-profiles/index.js';
 import { ALOY_REVIEWED_TEAMS } from '../data/team-profiles/aloy-reviewed.js';
 import { AMBER_REVIEWED_TEAMS } from '../data/team-profiles/amber-reviewed.js';
+import { ARATAKI_ITTO_REVIEWED_TEAMS } from '../data/team-profiles/arataki-itto-reviewed.js';
 
 registerReviewedTeams(ALOY_REVIEWED_TEAMS);
 registerReviewedTeams(AMBER_REVIEWED_TEAMS);
+registerReviewedTeams(ARATAKI_ITTO_REVIEWED_TEAMS);
