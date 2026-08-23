0f0869c8800024fe29fb0a5fbecf0cab8557f3fe
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-23 17:03:48 +0800

    Register Amber through reviewed character bootstrap

--- js/features/aloy-reviewed-bootstrap.js
+++ js/features/aloy-reviewed-bootstrap.js
@@ -1,4 +1,6 @@
 import { registerReviewedTeams } from '../data/team-profiles/index.js';
 import { ALOY_REVIEWED_TEAMS } from '../data/team-profiles/aloy-reviewed.js';
+import { AMBER_REVIEWED_TEAMS } from '../data/team-profiles/amber-reviewed.js';
 
 registerReviewedTeams(ALOY_REVIEWED_TEAMS);
+registerReviewedTeams(AMBER_REVIEWED_TEAMS);
