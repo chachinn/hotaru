b3b295c109ddf234afcf426cd153365399849a20
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-23 17:15:18 +0800

    Review Amber character builds and Smart Team compatibility (#70)
    * Add reviewed Amber build profile
    
    * Add reviewed Amber team library
    
    * Add Amber full-roster compatibility policy
    
    * Register reviewed Amber build profile
    
    * Register Amber reviewed teams at startup
    
    * Register Amber through reviewed character bootstrap

--- js/features/amber-reviewed-bootstrap.js
+++ js/features/amber-reviewed-bootstrap.js
@@ -1,6 +1,3 @@
 import { registerReviewedTeams } from '../data/team-profiles/index.js';
-import { ALOY_REVIEWED_TEAMS } from '../data/team-profiles/aloy-reviewed.js';
 import { AMBER_REVIEWED_TEAMS } from '../data/team-profiles/amber-reviewed.js';
-
-registerReviewedTeams(ALOY_REVIEWED_TEAMS);
 registerReviewedTeams(AMBER_REVIEWED_TEAMS);
