31562401486cffdf7e51e81428ad85cadc7c2a4a
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-23 17:03:05 +0800

    Register Amber reviewed teams at startup

---
+++ js/features/amber-reviewed-bootstrap.js
@@ -0,0 +1,3 @@
+import { registerReviewedTeams } from '../data/team-profiles/index.js';
+import { AMBER_REVIEWED_TEAMS } from '../data/team-profiles/amber-reviewed.js';
+registerReviewedTeams(AMBER_REVIEWED_TEAMS);
