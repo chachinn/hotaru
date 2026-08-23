bdaa8b435295540277d2da966a1f38abcc397cc7
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-23 18:22:32 +0800

    Register Chiori reviewed teams at startup

--- js/features/aloy-reviewed-bootstrap.js
+++ js/features/aloy-reviewed-bootstrap.js
@@ -4,9 +4,11 @@ import { AMBER_REVIEWED_TEAMS } from '../data/team-profiles/amber-reviewed.js';
 import { ARATAKI_ITTO_REVIEWED_TEAMS } from '../data/team-profiles/arataki-itto-reviewed.js';
 import { BAIZHU_REVIEWED_TEAMS } from '../data/team-profiles/baizhu-reviewed.js';
 import { CHASCA_REVIEWED_TEAMS } from '../data/team-profiles/chasca-reviewed.js';
+import { CHIORI_REVIEWED_TEAMS } from '../data/team-profiles/chiori-reviewed.js';
 
 registerReviewedTeams(ALOY_REVIEWED_TEAMS);
 registerReviewedTeams(AMBER_REVIEWED_TEAMS);
 registerReviewedTeams(ARATAKI_ITTO_REVIEWED_TEAMS);
 registerReviewedTeams(BAIZHU_REVIEWED_TEAMS);
 registerReviewedTeams(CHASCA_REVIEWED_TEAMS);
+registerReviewedTeams(CHIORI_REVIEWED_TEAMS);
