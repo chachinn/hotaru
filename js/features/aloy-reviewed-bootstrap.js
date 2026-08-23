6085916d0cdca06ba5ef052666d8ee7689a51fef
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-23 16:52:48 +0800

    Register Aloy reviewed teams at startup

---
+++ js/features/aloy-reviewed-bootstrap.js
@@ -0,0 +1,4 @@
+import { registerReviewedTeams } from '../data/team-profiles/index.js';
+import { ALOY_REVIEWED_TEAMS } from '../data/team-profiles/aloy-reviewed.js';
+
+registerReviewedTeams(ALOY_REVIEWED_TEAMS);
